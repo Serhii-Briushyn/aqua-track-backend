a26da23a1d06e9fa993e96da43a8b18387367866
Yuliia Voziian <vozianulia@gmail.com>
2025-01-10 21:57:52 +0200

    update

--- src/controllers/water.js
+++ src/controllers/water.js
@@ -4,7 +4,7 @@ import { updateWaterById } from '../services/water.js';
 import { deleteWaterById } from '../services/water.js';
 import { getDailyWater, getMonthlyWater } from '../services/water.js';
 import createHttpError from 'http-errors';
-import mongoose from 'mongoose';
+
 
 // create
 export const createWaterController = async (req, res) => {
@@ -28,96 +28,77 @@ export const createWaterController = async (req, res) => {
 
 // update
 export const updateWaterController = async (req, res, next) => {
-    try {
-        const { id } = req.params;
 
-        if (!mongoose.isValidObjectId(id)) {
-            throw createHttpError(400, 'Invalid water record ID');
-        }
+    const { id } = req.params;
 
-        const userId = req.user.id;
-        const data = { ...req.body };
+    const userId = req.user.id;
+    const data = { ...req.body };
 
-        const updatedWater = await updateWaterById(id, userId, data);
+    const updatedWater = await updateWaterById(id, userId, data);
 
-        if (!updatedWater) {
-            throw createHttpError(404, "Water record not found");
-        }
-
-        res.status(200).json({
-            status: 200,
-            message: "Successfully updated the water record!",
-            data: updatedWater,
-        });
-    } catch (error) {
-        next(error);
+    if (!updatedWater) {
+        throw createHttpError(404, "Water record not found");
     }
+
+    res.status(200).json({
+        status: 200,
+        message: "Successfully updated the water record!",
+        data: updatedWater,
+    });
 };
 
 //delete
 
 export const deleteWaterController = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const userId = req.user.id;
 
-        const water = await deleteWaterById(id, userId);
+    const { id } = req.params;
+    const userId = req.user.id;
 
-        if (!water) {
-            throw createHttpError(404, "Water record not found");
-        }
+    const water = await deleteWaterById(id, userId);
 
-        res.status(200).json({
-            status: 200,
-            message: "Successfully deleted the water record!",
-            data: water,
-        });
-    } catch (error) {
-        next(error);
+    if (!water) {
+        throw createHttpError(404, "Water record not found");
     }
+
+    res.status(204).json();
 };
 
 // Get daily water consumption
 
 export const getDailyWaterController = async (req, res, next) => {
-    try {
-        const userId = req.user.id;
-        const { date } = req.query;
 
-        if (!date) {
-            throw createHttpError(400, 'The "date" query parameter is required.');
-        }
+    const userId = req.user.id;
+    const { date } = req.query;
 
-        const result = await getDailyWater(userId, date);
-
-        res.status(200).json({
-            status: 200,
-            message: "Daily water consumption data retrieved successfully.",
-            ...result,
-        });
-    } catch (error) {
-        next(error);
+    if (!date) {
+        throw createHttpError(400, 'The "date" query parameter is required.');
     }
+
+    const result = await getDailyWater(userId, date);
+
+    res.status(200).json({
+        status: 200,
+        message: "Daily water consumption data retrieved successfully.",
+        ...result,
+    });
 };
 
 // Get monthly water consumption
 export const getMonthlyWaterController = async (req, res, next) => {
-    try {
-        const userId = req.user.id;
-        const { month, year } = req.query;
 
-        if (!month || !year) {
-            throw createHttpError(400, 'The "month" and "year" query parameters are required.');
-        }
+    const userId = req.user.id;
+    const { month, year } = req.query;
 
-        const result = await getMonthlyWater(userId, month, year);
-
-        res.status(200).json({
-            status: 200,
-            message: "Monthly water consumption data retrieved successfully.",
-            data: result,
-        });
-    } catch (error) {
-        next(error);
+    if (!month || !year) {
+        throw createHttpError(400, 'The "month" and "year" query parameters are required.');
     }
+
+    const result = await getMonthlyWater(userId, month, year);
+
+    res.status(200).json({
+        status: 200,
+        message: "Monthly water consumption data retrieved successfully.",
+        data: result,
+    });
+
 };
