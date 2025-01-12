ef963c430cde88ce7183ac2a2d156ef28f01d6cf
Serhii Briushyn <serhii.briushyn@icloud.com>
2025-01-12 18:38:34 +0100

    origin cors

--- src/server.js
+++ src/server.js
@@ -20,7 +20,10 @@ export const setupServer = () => {
 
   app.use(
     cors({
-      origin: env("ALLOWED_ORIGIN") || "http://localhost:5173",
+      origin: [
+        "http://localhost:5173",
+        "https://aqua-track-frontend-rouge.vercel.app",
+      ],
       credentials: true,
     }),
   );
